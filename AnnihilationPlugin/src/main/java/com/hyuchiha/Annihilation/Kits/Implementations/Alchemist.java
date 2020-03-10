9673b56e19e767694c2dd1a1a8d3fad660eebd8c
hyuchiha <kev.pachec.lis@gmail.com>
2020-03-09 21:14:45 -0600

    Ender Alchemist drop

--- AnnihilationPlugin/src/main/java/com/hyuchiha/Annihilation/Kits/Implementations/Alchemist.java
+++ AnnihilationPlugin/src/main/java/com/hyuchiha/Annihilation/Kits/Implementations/Alchemist.java
@@ -6,6 +6,7 @@ import com.hyuchiha.Annihilation.Kits.Base.BaseKit;
 import com.hyuchiha.Annihilation.Manager.PlayerManager;
 import com.hyuchiha.Annihilation.Messages.Translator;
 import com.hyuchiha.Annihilation.Utils.KitUtils;
+import com.hyuchiha.Annihilation.Utils.TimersUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -19,19 +20,82 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.HashMap;
-import java.util.UUID;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public class Alchemist extends BaseKit {
   private final HashMap<UUID, Location> personalBrewingLocations;
+  private final HashMap<Integer, List<ItemStack>> drops;
+  private final List<Integer> probs;
+  private final Random random = new Random();
 
   // https://shotbow.net/forum/wiki/anni-alchemist/
   public Alchemist(String name, ItemStack icon, ConfigurationSection section) {
     super(name, icon, section);
 
     personalBrewingLocations = new HashMap<>();
+    drops = new HashMap<>();
+    probs = new ArrayList<>();
+
+    fillDrops();
+    fillProbs();
+  }
+
+  private void fillDrops() {
+    // 32% drops
+    List<ItemStack> drops32 = new ArrayList<>();
+    drops32.add(new ItemStack(Material.FERMENTED_SPIDER_EYE));
+    drops32.add(new ItemStack(Material.NETHER_WARTS));
+    drops.put(32, drops32);
+
+    // 28% drops
+    List<ItemStack> drop28 = new ArrayList<>();
+    drop28.add(new ItemStack(Material.SPECKLED_MELON));
+    drop28.add(new ItemStack(Material.GOLDEN_CARROT));
+    drop28.add(new ItemStack(Material.SUGAR));
+    drop28.add(new ItemStack(Material.SPIDER_EYE));
+    drop28.add(new ItemStack(Material.MAGMA_CREAM));
+    drops.put(28, drop28);
+
+    // 15% drops
+    List<ItemStack> drop15 = new ArrayList<>();
+    drop15.add(new ItemStack(Material.GLOWSTONE_DUST));
+    drop15.add(new ItemStack(Material.GOLDEN_CARROT));
+    drop15.add(new ItemStack(Material.GOLDEN_APPLE));
+    drops.put(15, drop15);
+
+    // 15% drops (bad drops)
+    List<ItemStack> drop15Bad = new ArrayList<>();
+    drop15Bad.add(new ItemStack(Material.ROTTEN_FLESH));
+    drop15Bad.add(new ItemStack(Material.POISONOUS_POTATO));
+    drop15Bad.add(new ItemStack(Material.SNOW_BALL));
+    drop15Bad.add(new ItemStack(Material.STRING));
+    drops.put(-15, drop15Bad);
+
+    // 7% drops
+    List<ItemStack> drop7 = new ArrayList<>();
+    drop7.add(new ItemStack(Material.GHAST_TEAR));
+    drops.put(7, drop7);
+
+    // 3% drops
+    List<ItemStack> drop3 = new ArrayList<>();
+    drop3.add(new ItemStack(Material.BLAZE_POWDER));
+    drop3.add(new ItemStack(Material.SULPHUR));
+    drops.put(3, drop3);
+
+  }
+
+  private void fillProbs() {
+    for (int key: drops.keySet()) {
+      int percent = Math.abs(key);
+
+      for (int i = 0; i < percent; i++) {
+        probs.add(key);
+      }
+    }
   }
 
   @Override
@@ -80,7 +144,7 @@ public class Alchemist extends BaseKit {
     GamePlayer gPlayer = PlayerManager.getGamePlayer(player);
 
     if (gPlayer.getKit() == Kit.ALCHEMIST && event.getBlock().getType() == Material.BREWING_STAND) {
-      this.personalBrewingLocations.put(player.getUniqueId(), event.getBlock().getLocation());
+      addBrewing(player, event.getBlock().getLocation());
     }
   }
 
@@ -152,6 +216,28 @@ public class Alchemist extends BaseKit {
     }
   }
 
+  @EventHandler
+  public void onAlchemistBookInteract(PlayerInteractEvent e) {
+    Player player = e.getPlayer();
+    GamePlayer gPlayer = PlayerManager.getGamePlayer(player);
+    Action action = e.getAction();
+
+    if (action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK) {
+      PlayerInventory inventory = player.getInventory();
+      ItemStack handItem = inventory.getItemInMainHand();
+
+      if (handItem != null && KitUtils.isKitItem(handItem, "KITS.ALCHEMIST_BOOK")) {
+        if (TimersUtils.hasExpired(player, gPlayer.getKit())) {
+          giveRandomDrop(player);
+          TimersUtils.addDelay(player, Kit.ALCHEMIST, 90, TimeUnit.SECONDS);
+        } else {
+          KitUtils.showKitItemDelay(player, gPlayer.getKit());
+        }
+      }
+    }
+
+  }
+
   private boolean isBrewingFromAlchemist(Location location) {
     for (Location brewing: this.personalBrewingLocations.values()) {
       if (brewing.equals(location)) {
@@ -175,6 +261,14 @@ public class Alchemist extends BaseKit {
     }
 
     this.personalBrewingLocations.remove(identifier);
+  }
+
+  private void giveRandomDrop(Player player) {
+    int position = random.nextInt(99);
+
+    List<ItemStack> percentDrops = drops.get(position);
+    int dropPosition = random.nextInt(percentDrops.size() - 1);
+    player.getInventory().addItem(percentDrops.get(dropPosition));
   }
 
 }
