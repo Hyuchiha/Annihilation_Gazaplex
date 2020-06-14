19e4952f7467bd536744df6066ef2446c99a77b3
Hyuchiha <kev.pachec.lis@gmail.com>
2020-06-14 14:14:32 -0500

    Some basic changes

--- v1_13_R1/src/main/java/com/hyuchiha/Annihilation/Mobs/v1_13_R1/NMS/CustomZombie.java
+++ v1_13_R1/src/main/java/com/hyuchiha/Annihilation/Mobs/v1_13_R1/NMS/CustomZombie.java
@@ -3,7 +3,7 @@ package com.hyuchiha.Annihilation.Mobs.v1_13_R1.NMS;
 import com.hyuchiha.Annihilation.Mobs.MobUtils;
 import net.minecraft.server.v1_13_R1.*;
 
-import java.util.List;
+import java.util.Set;
 
 public class CustomZombie extends EntityZombie {
   public CustomZombie(World world) {
@@ -12,13 +12,13 @@ public class CustomZombie extends EntityZombie {
 
   @Override
   protected void n() {
-    List goalB = (List) MobUtils.getPrivateField("b", PathfinderGoalSelector.class, goalSelector);
+    Set goalB = (Set) MobUtils.getPrivateField("b", PathfinderGoalSelector.class, goalSelector);
     goalB.clear();
-    List goalC = (List) MobUtils.getPrivateField("c", PathfinderGoalSelector.class, goalSelector);
+    Set goalC = (Set) MobUtils.getPrivateField("c", PathfinderGoalSelector.class, goalSelector);
     goalC.clear();
-    List targetB = (List) MobUtils.getPrivateField("b", PathfinderGoalSelector.class, targetSelector);
+    Set targetB = (Set) MobUtils.getPrivateField("b", PathfinderGoalSelector.class, targetSelector);
     targetB.clear();
-    List targetC = (List) MobUtils.getPrivateField("c", PathfinderGoalSelector.class, targetSelector);
+    Set targetC = (Set) MobUtils.getPrivateField("c", PathfinderGoalSelector.class, targetSelector);
     targetC.clear();
 
     this.goalSelector.a(0, new PathfinderGoalFloat(this));
--- v1_13_R2/src/main/java/com/hyuchiha/Annihilation/Mobs/v1_13_R2/NMS/CustomWither.java
+++ v1_13_R2/src/main/java/com/hyuchiha/Annihilation/Mobs/v1_13_R2/NMS/CustomWither.java
@@ -4,7 +4,7 @@ import com.hyuchiha.Annihilation.Mobs.MobUtils;
 import com.hyuchiha.Annihilation.Mobs.v1_13_R2.Pathfinders.PathfinderGoalWalkToLocation;
 import net.minecraft.server.v1_13_R2.*;
 
-import java.util.List;
+import java.util.Set;
 
 public class CustomWither extends EntityWither {
 
@@ -14,13 +14,13 @@ public class CustomWither extends EntityWither {
 
   @Override
   protected void n() {
-    List goalB = (List) MobUtils.getPrivateField("b", PathfinderGoalSelector.class, goalSelector);
+    Set goalB = (Set) MobUtils.getPrivateField("b", PathfinderGoalSelector.class, goalSelector);
     goalB.clear();
-    List goalC = (List) MobUtils.getPrivateField("c", PathfinderGoalSelector.class, goalSelector);
+    Set goalC = (Set) MobUtils.getPrivateField("c", PathfinderGoalSelector.class, goalSelector);
     goalC.clear();
-    List targetB = (List) MobUtils.getPrivateField("b", PathfinderGoalSelector.class, targetSelector);
+    Set targetB = (Set) MobUtils.getPrivateField("b", PathfinderGoalSelector.class, targetSelector);
     targetB.clear();
-    List targetC = (List) MobUtils.getPrivateField("c", PathfinderGoalSelector.class, targetSelector);
+    Set targetC = (Set) MobUtils.getPrivateField("c", PathfinderGoalSelector.class, targetSelector);
     targetC.clear();
 
 
