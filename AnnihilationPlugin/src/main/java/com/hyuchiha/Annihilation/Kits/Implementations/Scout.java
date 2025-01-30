b5af485bda4feff3ffcbd96900552df1b4ba3f0b
hyuchiha <kev.pachec.lis@gmail.com>
2025-01-29 23:35:38 -0600

    Removed old code of Scout

--- AnnihilationPlugin/src/main/java/com/hyuchiha/Annihilation/Kits/Implementations/Scout.java
+++ AnnihilationPlugin/src/main/java/com/hyuchiha/Annihilation/Kits/Implementations/Scout.java
@@ -84,7 +84,7 @@ public class Scout extends BaseKit {
             }
 
             if (TimersUtils.hasExpired(player, Kit.SCOUT)) {
-                TimersUtils.addDelay(player, Kit.SCOUT, 5, TimeUnit.SECONDS);
+                TimersUtils.addDelay(player, Kit.SCOUT, 1, TimeUnit.SECONDS);
                 Location hookLoc = event.getHook().getLocation();
                 Location playerLoc = player.getLocation();
 
@@ -113,42 +113,6 @@ public class Scout extends BaseKit {
                 player.setVelocity(vel);
                 XSound.ENTITY_PLAYER_ATTACK_SWEEP.play(player, 1.0F, 1.0F);
             }
-
-//            if (event.getState() == PlayerFishEvent.State.IN_GROUND) {
-//
-//                Location hookLoc = event.getHook().getLocation();
-//                Location playerLoc = player.getLocation();
-//
-//                double hookX = (int) hookLoc.getX();
-//                double hookY = (int) hookLoc.getY();
-//                double hookZ = (int) hookLoc.getZ();
-//
-//                Material inType = hookLoc.getWorld().getBlockAt(hookLoc).getType();
-//                if (inType == Material.AIR || inType == Material.WATER || inType == Material.LAVA) {
-//                    Material belowType = hookLoc.getWorld().getBlockAt((int) hookX, (int) (hookY - 0.1), (int) hookZ).getType();
-//                    if (belowType == Material.AIR || belowType == Material.WATER || belowType == Material.LAVA) {
-//                        return;
-//                    }
-//                }
-//
-//                playerLoc.setY(playerLoc.getY() + 0.5);
-//                player.teleport(playerLoc);
-//
-//                Vector diff = hookLoc.toVector().subtract(playerLoc.toVector());
-//                Vector vel = new Vector();
-//                double d = hookLoc.distance(playerLoc);
-//                vel.setX((1.0 + 0.07 * d) * diff.getX() / d);
-//                vel.setY((1.0 + 0.03 * d) * diff.getY() / d + 0.04 * d);
-//                vel.setZ((1.0 + 0.07 * d) * diff.getZ() / d);
-//                player.setVelocity(vel);
-//
-//                // Play the initial left-click sound when throwing the grappling hook
-//                XSound.ENTITY_PLAYER_ATTACK_SWEEP.play(player, 1.0F, 1.0F);
-//            } else if (event.getState() == PlayerFishEvent.State.CAUGHT_ENTITY) {
-//
-//                // Play the sound when the hook is grabbed
-//                XSound.ENTITY_ENDER_PEARL_THROW.play(player, 1.0F, 1.0F);
-//            }
         }
     }
 }
